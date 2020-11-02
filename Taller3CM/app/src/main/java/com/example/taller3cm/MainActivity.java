f25c464a54424975beb237009e2542203f5370b5
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-11-02 18:19:32 -0500

    Added Validation

--- Taller3CM/app/src/main/java/com/example/taller3cm/MainActivity.java
+++ Taller3CM/app/src/main/java/com/example/taller3cm/MainActivity.java
@@ -51,6 +51,50 @@ public class MainActivity extends AppCompatActivity {
         updateUI(currentUser);
 }
 
+    private void loginUser() {
+        String email = edtUser.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        boolean validEmail = Utils.validateEmail(email);
+        boolean validPass = Utils.validatePassword(password);
+
+        if (!validEmail) {
+            if (email.isEmpty()) {
+                edtUser.setError("Requerido");
+            } else {
+                edtUser.setError("E-mail no válido");
+            }
+        } else edtUser.setError(null);
+        if (!validPass) {
+            if (password.isEmpty()) {
+                edtPassword.setError("Requerido");
+            } else {
+                edtPassword.setError("6 caracteres mínimos");
+            }
+        } else edtPassword.setError(null);
+
+        if (validEmail && validPass) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+                        }
+                    });
+        }
+    }
+
     private void updateUI(FirebaseUser currentUser) {
         if(currentUser != null)
         {
@@ -61,31 +105,5 @@ public class MainActivity extends AppCompatActivity {
             edtUser.setText("");
             edtPassword.setText("");
         }
-    }
-
-    private void loginUser() {
-        String email = edtUser.getText().toString();
-        String password = edtPassword.getText().toString();
-
-        //Validate
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
     }
 }
