1adb1fb7bf2f525d2d40eff89c09ed7c9b24c48c
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-11-02 19:02:41 -0500

    Login and Logout Done

--- Taller3CM/app/src/main/java/com/example/taller3cm/MapActivity.java
+++ Taller3CM/app/src/main/java/com/example/taller3cm/MapActivity.java
@@ -1,14 +1,43 @@
 package com.example.taller3cm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MapActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuprincipal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menuLogout)
+        {
+            mAuth.signOut();
+            Intent intent = new Intent(MapActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+
+        return true;
     }
 }
