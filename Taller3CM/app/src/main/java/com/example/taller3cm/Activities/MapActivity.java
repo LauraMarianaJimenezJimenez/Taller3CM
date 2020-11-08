48d4964d62b319b7b8d670255bb4873d29f317b7
M <monik9912@hotmail.com>
2020-11-08 18:57:06 -0500

    Fixed project

---
+++ Taller3CM/app/src/main/java/com/example/taller3cm/Activities/MapActivity.java
@@ -0,0 +1,54 @@
+package com.example.taller3cm.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.taller3cm.Other.Usuario;
+import com.example.taller3cm.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MapActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuprincipal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menuLogout)
+        {
+            mAuth.signOut();
+            Intent intent = new Intent(MapActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+        if(id == R.id.menuDisponible)
+        {
+
+        }
+        if(id == R.id.menuListaDisponibles)
+        {
+            Intent intent = new Intent(MapActivity.this, AvailableListActivity.class);
+            startActivity(intent);
+        }
+
+        return true;
+    }
+}
