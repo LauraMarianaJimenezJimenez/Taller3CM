41e4982746e673f3c287e0b8644d4b601d071987
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-11-10 21:28:45 -0500

    Clear Activities

--- Taller3CM/app/src/main/java/com/example/taller3cm/Activities/MapActivity.java
+++ Taller3CM/app/src/main/java/com/example/taller3cm/Activities/MapActivity.java
@@ -43,6 +43,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -67,7 +68,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     DatabaseReference myRef;
     private FirebaseAuth mAuth;
     ArrayList<Localizacion> locations = new ArrayList<>();
-
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,6 +103,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        user = mAuth.getCurrentUser();
+
+        if(user == null){
+            Intent intent = new Intent(MapActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+
+        }
     }
 
     @Override
